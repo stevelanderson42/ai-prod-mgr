c69cbd46176eb13d920c320fd0ca28b8f4cae441
stevelanderson42 <stevelanderson.42@gmail.com>
2025-11-27 14:23:14 -0700

    test for verification index

--- learning/Chapter1_basics.py
+++ learning/Chapter1_basics.py
@@ -10,4 +10,6 @@ print('What is your age?')    # ask for their age
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
 len('hello')
+print('hello big world')
+print('What is your name?') # ask for their name
 
